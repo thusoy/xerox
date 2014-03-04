6b704182a5521b08c86f765c866dd318e0c58ffb
Tarjei Husøy <tarjei@roms.no>
2014-03-04 15:43:28 +0100

    Remove any dependencies on windows by using ctypes
    Removes the need for pywin32, Tkinter or similar.
    
    Add some more test cases as well.
    
    Tested on win7 x64 with 32bit python (2.6, 2.7, 3.3).

--- test_xerox.py
+++ test_xerox.py
@@ -5,16 +5,22 @@ import xerox
 import unittest
 
 class BasicAPITestCase(unittest.TestCase):
+
     def setUp(self):
         self.text = 'And now for something completely different.'
         
-    def test_copy(self):
+    def test_copy_paste(self):
         xerox.copy(self.text)
         self.assertEqual(xerox.paste(), self.text)
-        
-    def test_paste(self):
-        xerox.copy(self.text)
-        self.assertEqual(xerox.paste(), self.text)
+
+    def test_unicode(self):
+        unicode_text = u'Ξεσκεπάζω τὴν ψυχοφθόρα βδελυγμία'
+        xerox.copy(unicode_text)
+        self.assertEqual(xerox.paste(), unicode_text)
+
+    def test_empty(self):
+        xerox.copy('')
+        self.assertEqual(xerox.paste(), '')
         
         
 if __name__ == '__main__':
--- xerox/win.py
+++ xerox/win.py
@@ -5,30 +5,28 @@
 
 from .base import * 
 
-try:
-    import win32clipboard as clip
-    import win32con
-except ImportError as why:
-    raise Pywin32NotFound
+import ctypes
 
+_CF_TEXT = 1
 
 def copy(string): 
     """Copy given string into system clipboard."""
-
-    clip.OpenClipboard()
-    clip.EmptyClipboard()
-    clip.SetClipboardData(win32con.CF_UNICODETEXT, string) 
-    clip.CloseClipboard()
-
-    return
+    GMEM_DDESHARE = 0x2000
+    ctypes.windll.user32.OpenClipboard(0)
+    ctypes.windll.user32.EmptyClipboard()
+    hCd = ctypes.windll.kernel32.GlobalAlloc(GMEM_DDESHARE, len(string.encode('utf-8')) + 1)
+    pchData = ctypes.windll.kernel32.GlobalLock(hCd)
+    ctypes.cdll.msvcrt.strcpy(ctypes.c_char_p(pchData), string.encode('utf-8'))
+    ctypes.windll.kernel32.GlobalUnlock(hCd)
+    ctypes.windll.user32.SetClipboardData(_CF_TEXT, hCd)
+    ctypes.windll.user32.CloseClipboard()
     
 
 def paste():
     """Returns system clipboard contents."""
-
-    clip.OpenClipboard() 
-    d = clip.GetClipboardData(win32con.CF_TEXT) 
-    clip.CloseClipboard() 
-    return d 
-
- 
+    ctypes.windll.user32.OpenClipboard(0)
+    pcontents = ctypes.windll.user32.GetClipboardData(_CF_TEXT)
+    data = ctypes.c_char_p(pcontents).value
+    #ctypes.windll.kernel32.GlobalUnlock(pcontents)
+    ctypes.windll.user32.CloseClipboard()
+    return data.decode('utf-8')
